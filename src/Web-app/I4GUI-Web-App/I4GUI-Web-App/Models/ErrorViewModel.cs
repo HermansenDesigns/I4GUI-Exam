f9ce6f5a8fd6d31f1686ce194373fda6810991ae
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-06-17 23:03:27 +0200

    Added Web api proj

---
+++ src/Web-app/I4GUI-Web-App/I4GUI-Web-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace I4GUI_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
