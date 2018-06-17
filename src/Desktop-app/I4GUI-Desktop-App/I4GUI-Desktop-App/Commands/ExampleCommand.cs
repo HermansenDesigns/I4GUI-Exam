f7dc521aee773d46f32de2b8e063db229e124a4c
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-06-17 23:30:56 +0200

    Added Desktop start

---
+++ src/Desktop-app/I4GUI-Desktop-App/I4GUI-Desktop-App/Commands/ExampleCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace I4GUI_Desktop_App.Commands
+{
+    public class ExampleCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+        }
+    }
+}
